4ea5483ca00b26c8c645fae4e32a24e16a1cacb4
vkarthikm <virat18mvk@gmail.com>
2025-02-17 10:28:37 +0530

    v1

--- server.js
+++ server.js
@@ -4,14 +4,41 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const linkRoutes = require("./routes/linkRoutes");
 require("dotenv").config();
+const axios = require("axios");
+const cron = require("node-cron");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 connectDB();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/links", linkRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// /live route
+app.get("/live", (req, res) => {
+  res.send("lecha ra pumka");
+});
+
+// Default route for unmatched requests
+app.use((req, res) => {
+  res.send("dobey ra");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Cron job to ping the server every 14 minutes
+cron.schedule("*/14 * * * *", () => {
+  axios.get("https://linktree-mg98.onrender.com/live")
+    .then(() => console.log("Server pinged to keep it active"))
+    .catch(err => console.error("Error pinging server:", err.message));
+});
